70dee0b610b91e2faf08f6e9984fe220eb5451bf
Buck Brady <buck@voidstack.io>
2022-07-23 03:16:44 -0600

    add app commands

--- src/install_command.sh
+++ src/install_command.sh
@@ -1,5 +1,9 @@
 setup_app "${args[appname]}"
 
-docker_compose_setup "${args[appname]}"
+if [ ! -f "apps/${args[appname]}/.setup-done" ]; then
+  touch "apps/${args[appname]}/.setup-done"
+  info "please view/update values for ${args[appname]} app in .env then rerun this command again"
+  exit 0
+fi
 
-docker_compose_up "${args[appname]}"
+docker_compose_setup "${args[appname]}"
