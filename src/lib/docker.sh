6e4074beb165d25db6058cc9cb8a7220cea90d22
Buck Brady <buck@voidstack.io>
2022-07-23 22:35:22 -0600

    adding more commands

--- src/lib/docker.sh
+++ src/lib/docker.sh
@@ -28,44 +28,44 @@ generate_compose_profiles() {
 
 docker_compose_setup() {
   compose_files=$(generate_compose_files_list)
-  env $(cat .env | grep -v "^#" | xargs ) docker compose --project-name labtastic --profile "$1-setup" ${compose_files} up -d
+  env $(cat .env | grep -v "^#" | grep -v "ENABLED_APPS" |  xargs ) docker compose --project-name labtastic --profile "$1-setup" ${compose_files} up -d
 }
 
 docker_compose_up() {
   compose_files=$(generate_compose_files_list)
-  env $(cat .env | grep -v "^#" | xargs ) docker compose --project-name labtastic --profile "$1" ${compose_files} up -d
+  env $(cat .env | grep -v "^#" | grep -v "ENABLED_APPS" | xargs ) docker compose --project-name labtastic --profile "$1" ${compose_files} up -d
 }
 
 docker_compose_down() {
   compose_files=$(generate_compose_files_list)
-  env $(cat .env | grep -v "^#" | xargs ) docker compose --project-name labtastic --profile "$1" ${compose_files} down
+  env $(cat .env | grep -v "^#" | grep -v "ENABLED_APPS" | xargs ) docker compose --project-name labtastic --profile "$1" ${compose_files} down
 }
 
 docker_compose_stop_all() {
   compose_files=$(generate_compose_files_list)
-  env $(cat .env | grep -v "^#" | xargs ) docker compose --project-name labtastic --profile "$app" ${compose_files} stop
+  env $(cat .env | grep -v "^#" | grep -v "ENABLED_APPS" | xargs ) docker compose --project-name labtastic --profile "$app" ${compose_files} stop
 }
 
 docker_compose_stop() {
   compose_files=$(generate_compose_files_list)
-  env $(cat .env | grep -v "^#" | xargs ) docker compose --project-name labtastic --profile "$1" ${compose_files} stop
+  env $(cat .env | grep -v "^#" | grep -v "ENABLED_APPS" | xargs ) docker compose --project-name labtastic --profile "$1" ${compose_files} stop
 }
 
 docker_compose_start_all() {
   compose_files=$(generate_compose_files_list)
-  env $(cat .env | grep -v "^#" | xargs ) docker compose --project-name labtastic --profile "$app" ${compose_files}l start
+  env $(cat .env | grep -v "^#" | grep -v "ENABLED_APPS" | xargs ) docker compose --project-name labtastic --profile "$app" ${compose_files}l start
   
 }
 
 docker_compose_start() {
   compose_files=$(generate_compose_files_list)
-  env $(cat .env | grep -v "^#" | xargs ) docker compose --project-name labtastic --profile "$1" ${compose_files} start
+  env $(cat .env | grep -v "^#" | grep -v "ENABLED_APPS" | xargs ) docker compose --project-name labtastic --profile "$1" ${compose_files} start
 }
 
 docker_compose_status() {
   compose_files=$(generate_compose_files_list)
   compose_profiles=$(generate_compose_profiles)
-  env $(cat .env | grep -v "^#" | xargs ) docker compose --project-name labtastic --profile ${compose_profiles} ${compose_files} status
+  env $(cat .env | grep -v "^#" | grep -v "ENABLED_APPS" | xargs ) docker compose --project-name labtastic --profile ${compose_profiles} ${compose_files} status
 }
 
 docker_compose_logs() {
